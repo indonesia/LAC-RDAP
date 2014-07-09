46bb09b7995f5e12abb4dcf6e4899e49f606efdf
Commander Kerfy <keifer.gonzalez@gmail.com>
2014-07-09 13:11:38 -0400

    Add all 350+ geojson points.

--- JavaScript/RDAP.js
+++ JavaScript/RDAP.js
@@ -3,7 +3,7 @@ var geojsonLayer = new L.GeoJSON();
 
 //Create Global Marker Properties
 var geojsonMarkerOptions = {
-    radius: 8,
+    radius: 4,
     fillColor: "#ff7800",
     color: "#000",
     weight: 1,
@@ -22,7 +22,7 @@ function getJson(data) {
 	}).addTo(map);	
 
 	//Add Marker Cluster
-	var geojsonMarkerOptions = new L.MarkerClusterGroup(geojsonLayer, { 
+	var geojsonMarkerOptions = new L.MarkerClusterGroup(data, { 
      		maxClusterRadius: 60,
 		iconCreateFunction: null,
      		spiderfyOnMaxZoom: true, 
@@ -34,7 +34,7 @@ function getJson(data) {
 
 //AJAX GeoJSON Import
 $.ajax({
-      url: "http://localhost:8080/geoserver/LAC/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=LAC:RDAP&maxFeatures=50&outputFormat=text/javascript&format_options=callback:getJson",
+      url: "http://localhost:8080/geoserver/LAC/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=LAC:RDAP&maxFeatures=450&outputFormat=text/javascript&format_options=callback:getJson",
       dataType: 'jsonp',
       jsonpCallback: 'getJson',
       contentType: 'application/json',
