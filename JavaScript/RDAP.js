3550cf08bd54a4e5f69bec5092a9836f23dd72ec
Commander Kerfy <keifer.gonzalez@gmail.com>
2014-07-09 15:10:21 -0400

    Add template for Marker Cluster and Pop-ups. The latter based off of QGIS2Leaf export.

--- JavaScript/RDAP.js
+++ JavaScript/RDAP.js
@@ -2,10 +2,10 @@
 var geojsonLayer = new L.GeoJSON();
 
 //Create Global Marker Properties
-var geojsonMarkerOptions = {
+var markers = {
     radius: 4,
     fillColor: "#ff7800",
-    color: "#000",
+    color: "#fff",
     weight: 1,
     opacity: 1,
     fillOpacity: 0.8
@@ -17,19 +17,27 @@ function getJson(data) {
 	//Add Markers to Map
 	L.geoJson(data, {
     		pointToLayer: function (feature, latlng) {
-        	return L.circleMarker(latlng, geojsonMarkerOptions);
+        	return L.circleMarker(latlng, markers);
     		}
 	}).addTo(map);	
 
-	//Add Marker Cluster
-	var geojsonMarkerOptions = new L.MarkerClusterGroup(data, { 
-     		maxClusterRadius: 60,
-		iconCreateFunction: null,
-     		spiderfyOnMaxZoom: true, 
-     		showCoverageOnHover: true,
-     		zoomToBoundsOnClick: true 
-	});map.addLayer(geojsonMarkerOptions);
+	//Adding Pop-ups
+	/*
+	function geojsonLayer(feature, layer) {
+		var popupContent = '<table><tr></tr><tr><th>Country</th><td>' + feature.properties.COUNTRY + '</td></tr><tr><th>City</th><td>' + feature.properties.CITY + '</td></tr><tr><th>First Name</th><td>' + feature.properties.FIRST_NAME + '</td></tr><tr><th>Last Name</th><td>' + feature.properties.LAST_NAME + '</td></tr><tr><th>Profession</th><td>' + feature.properties.PROFESSION + '</td></tr><tr><th>Expert</th><td>' + feature.properties.EXPERT + '</td></tr><tr><th>Home</th><td>' + feature.properties.HOME + '</td></tr><tr><th>Office</th><td>' + feature.properties.OFFICE + '</td></tr><tr><th>Phone</th><td>' + feature.properties.CELULAR + '</td></tr><tr><th>E-mail</th><td>' + feature.properties.EMAIL + '</td></tr></table>';
+	layer.bindPopup(popupContent);
+	}
+	*/
 
+	//Add Marker Cluster
+	/*
+	var markers = new L.MarkerClusterGroup(data, { 
+     		maxClusterRadius: 100,
+     		spiderfyOnMaxZoom: true, 
+     		showCoverageOnHover: false,
+     		zoomToBoundsOnClick: true 
+	});map.addLayer(markers);
+	*/
 };
 
 //AJAX GeoJSON Import
@@ -43,5 +51,5 @@ $.ajax({
 
 //Add Data to Map
 map.addLayer(geojsonLayer);
-map.fitBounds(feature_group.getBounds());
+
 
