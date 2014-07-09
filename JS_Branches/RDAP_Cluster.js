7cc82d40dce4bc072ad42f4dae51e13d84fa0792
Commander Kerfy <keifer.gonzalez@gmail.com>
2014-07-09 10:38:41 -0400

    Updating to prep for clustering.

--- JS_Branches/RDAP_Cluster.js
+++ JS_Branches/RDAP_Cluster.js
@@ -5,6 +5,13 @@ function getJson(data) {
      geojsonLayer.addData(data);
 };
 
+//Cluster Function
+var markers = new L.MarkerClusterGroup();
+markers.addLayer(new L.Marker(getRandomLatLng(map)));
+... Add more layers ...
+map.addLayer(markers);
+
+
 //AJAX GeoJSON Import
 $.ajax({
       url: "http://localhost:8080/geoserver/LAC/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=LAC:RDAP&maxFeatures=50&outputFormat=text/javascript&format_options=callback:getJson",
---
+++ JavaScript/RDAP.js
@@ -0,0 +1,47 @@
+//Begin JavaScript
+var geojsonLayer = new L.GeoJSON();
+
+//Create Global Marker Properties
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+//Defining Geo-Data Function
+function getJson(data) {
+
+	//Add Markers to Map
+	L.geoJson(data, {
+    		pointToLayer: function (feature, latlng) {
+        	return L.circleMarker(latlng, geojsonMarkerOptions);
+    		}
+	}).addTo(map);	
+
+	//Add Marker Cluster
+	var geojsonMarkerOptions = new L.MarkerClusterGroup(geojsonLayer, { 
+     		maxClusterRadius: 60,
+		iconCreateFunction: null,
+     		spiderfyOnMaxZoom: true, 
+     		showCoverageOnHover: true,
+     		zoomToBoundsOnClick: true 
+	});map.addLayer(geojsonMarkerOptions);
+
+};
+
+//AJAX GeoJSON Import
+$.ajax({
+      url: "http://localhost:8080/geoserver/LAC/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=LAC:RDAP&maxFeatures=50&outputFormat=text/javascript&format_options=callback:getJson",
+      dataType: 'jsonp',
+      jsonpCallback: 'getJson',
+      contentType: 'application/json',
+      success: getJson
+});
+
+//Add Data to Map
+map.addLayer(geojsonLayer);
+map.fitBounds(feature_group.getBounds());
+
